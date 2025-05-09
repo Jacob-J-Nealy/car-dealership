ab234dd5ff4330ccd934bc74dee45274afb7f0bc
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-09 15:48:23 -0400

    Made Vehicle Constructor for Vehicle Class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -10,6 +10,21 @@ public class Vehicle {
     private String vehicleType;
     private String color;
     private int    odometer;
+    private double price;
+
+    // Vehicle Constructor
+    public Vehicle(int vin, int year, String make,
+                   String model, String vehicleType,
+                   String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
 
     
 
