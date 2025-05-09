9618526b713ca8f9246edf03ba89bd95ed5d365b
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-09 14:04:16 -0400

    Made Constructor for Dealership Class

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+
+    //Constructor
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+
+        private void getVehiclesByPrice() {
+
+        }
+
+
+    }
+}
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,4 @@
 package com.pluralsight;
 
-public class Test {
+public class Vehicle {
 }
