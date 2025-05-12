d6c251e149b866d389f113f55107cacd09c10af8
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-12 02:37:15 -0400

    Generated Getters and Setters inside of Vehicle Class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -46,21 +46,70 @@ public class Vehicle {
         vehicleBuilder.append("|");
         vehicleBuilder.append(String.format("%.2f", price));
         return vehicleBuilder.toString();
+    }
 
+    // Vehicle Getters and Setters
+    public int getVin() {
+        return vin;
+    }
 
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
 
+    public int getYear() {
+        return year;
+    }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
 
+    public String getMake() {
+        return make;
+    }
 
-        return "Vehicle{" +
-                "vin=" + vin +
-                ", year=" + year +
-                ", make='" + make + '\'' +
-                ", model='" + model + '\'' +
-                ", vehicleType='" + vehicleType + '\'' +
-                ", color='" + color + '\'' +
-                ", odometer=" + odometer +
-                ", price=" + price +
-                '}';
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
