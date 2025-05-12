acd2d8e8ebe78f0ed46f1a092251b69f19353507
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-12 12:57:51 -0400

    Working on getDealership() method inside of DealershipFileManager Class

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -3,31 +3,36 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-private static final String FILE_NAME = "inventory.csv";
-
-
 public class DealershipFileManager {
 
-//    String line;
-//    try {
-//        BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_NAME));
-//        while ((line = bufferedReader.readLine()) != null) {
-//            String[] vehicleAttributesParts = line.split("\\|");
-//            int vin            = Integer.parseInt(vehicleAttributesParts[0]);
-//            int year           = Integer.parseInt(vehicleAttributesParts[1]);
-//            String make        = vehicleAttributesParts[2];
-//            String model       = vehicleAttributesParts[3];
-//            String vehicleType = vehicleAttributesParts[4];
-//            String color       = vehicleAttributesParts[5];
-//            int odometer       = Integer.parseInt(vehicleAttributesParts[6]);
-//            double price       = Double.parseDouble(vehicleAttributesParts[7]);
-//            inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
-//        }
-//        bufferedReader.close();
-//    } catch (Exception e) {
-//        System.err.println("Couldn't Load Inventory");
-//    }
-//
+    private static final String FILE_NAME = "inventory.csv";
+
+    public Dealership getDealership() {
+        String line;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(FILE_NAME));
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] vehicleAttributesParts = line.split("\\|");
+                int vin            = Integer.parseInt(vehicleAttributesParts[0]);
+                int year           = Integer.parseInt(vehicleAttributesParts[1]);
+                String make        = vehicleAttributesParts[2];
+                String model       = vehicleAttributesParts[3];
+                String vehicleType = vehicleAttributesParts[4];
+                String color       = vehicleAttributesParts[5];
+                int odometer       = Integer.parseInt(vehicleAttributesParts[6]);
+                double price       = Double.parseDouble(vehicleAttributesParts[7]);
+                inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
+        }
+            bufferedReader.close();
+    }   catch (Exception e) {
+            System.err.println("Couldn't Load Inventory");
+    }
+    }
+
+    private void saveDealership() {
+
+    }
+
 
 
 }
