e70a798be9951138cf923eb79d8c15a734723f8d
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-05-14 15:27:30 -0400

    Finished Phase 4

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -3,9 +3,13 @@ package com.pluralsight;
 public class Program {
     public static void main(String[] args) {
 
+        // Initializing a New UserInterface
         UserInterface dealershipInterface = new UserInterface();
 
+        // Displaying the Dealership Interface
         dealershipInterface.display();
+
+        
 
 
     }
